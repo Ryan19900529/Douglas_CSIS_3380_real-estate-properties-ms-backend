724b615c39da5e9724672f3853dea3fd46e49ffb
Ryan <ab00999819@gmail.com>
2023-09-09 15:43:41 -0700

    update allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,10 @@
 const allowedOrigins = [
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://localhost:3000',
-    'http://localhost:5000',
-    'https://real-estate-properties-ms-frontend.onrender.com'
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+  "http://localhost:3000",
+  "http://localhost:5000",
+  "https://real-estate-properties-ms-frontend.onrender.com",
+  "https://javascript-real-estate-property-7i3r.onrender.com",
 ];
 
 module.exports = allowedOrigins;
